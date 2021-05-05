1f04c42f1e2d012f37f15cf79fdbb58c0f1bff20
mrudultora <mrudultora@gmail.com>
2021-05-05 18:09:12 +0530

    Added border in views in color picker popup layout

--- colorpicker/src/main/java/com/mrudultora/colorpicker/ColorPickerPopUp.java
+++ colorpicker/src/main/java/com/mrudultora/colorpicker/ColorPickerPopUp.java
@@ -7,7 +7,6 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.GradientDrawable;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
