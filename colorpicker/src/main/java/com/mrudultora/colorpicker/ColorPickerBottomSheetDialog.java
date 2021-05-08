1dd310a9227d6f787049f88cc3fb071c4614cf4c
Tarekk Mohamed Abdalla <tarekkma@gmail.com>
2021-05-08 16:49:40 +0200

    Add ColorPickerBottomSheetDialog.Builder

--- colorpicker/src/main/java/com/mrudultora/colorpicker/ColorPickerBottomSheetDialog.java
+++ colorpicker/src/main/java/com/mrudultora/colorpicker/ColorPickerBottomSheetDialog.java
@@ -43,6 +43,9 @@ import java.util.HashMap;
  * @since 3 May, 2021
  */
 public class ColorPickerBottomSheetDialog implements OnColorItemClickListener {
+
+    private final Context context;
+
     private final RecyclerView recyclerViewColors;
     private final View bottomSheetDialogView;
     private final RelativeLayout colorPaletteRelLayout;
@@ -50,34 +53,76 @@ public class ColorPickerBottomSheetDialog implements OnColorItemClickListener {
     private final View dividerView;
     private final AppCompatButton positiveButton;
     private final AppCompatButton negativeButton;
-    private boolean cardSizeChanged = false;
-    private boolean tickSizeChanged = false;
-    private boolean positiveButtonTextChanged = false;
-    private boolean negativeButtonTextChanged = false;
-    private boolean titleTextChanged = false;
-    private int columns = 5;
-    private int defaultColor = 0;
-    private int itemDrawableRes = 0;
-    private int tickColor = Color.WHITE;
-    private int dividerViewColor = 0;
-    private ColorItemShape colorShape = ColorItemShape.SQUARE;
-    private final Context context;
-    private final ArrayList<ColorPaletteItemModel> colorsList;
-    private HashMap<Integer,Integer> colorItems;
-    private String dialogTitle;
-    private String dialogPositiveButtonText;
-    private String dialogNegativeButtonText;
-    private OnDirectSelectColorListener directSelectColorListener;
-    private OnSelectColorListener selectColorListener;
     private BottomSheetDialog bottomSheetDialog;
-    private ColorAdapter colorAdapter;
-    private int selectedColorPosition = -1;
-    private float tickSizeDimen = 0f;                 // when equals 0 (default used would be 24dp)
-    private float cardViewDimen = 0f;                 // when equals 0 (default used would be 45dp)
 
-    public ColorPickerBottomSheetDialog(Context context) {
+    private ColorAdapter colorAdapter;
+
+    private  int selectedColorPosition = -1;
+
+    private final boolean cardSizeChanged ;
+    private final boolean tickSizeChanged;
+    private final boolean positiveButtonTextChanged;
+    private final boolean negativeButtonTextChanged;
+    private final boolean titleTextChanged;
+    private final int columns;
+    private final int defaultColor;
+    private final int itemDrawableRes;
+    private final int tickColor;
+    private final int dividerViewColor;
+    private final ColorItemShape colorShape;
+    private final ArrayList<ColorPaletteItemModel> colorsList;
+    private final HashMap<Integer, Integer> colorItems;
+    private final String dialogTitle;
+    private final String dialogPositiveButtonText;
+    private final String dialogNegativeButtonText;
+    private final OnDirectSelectColorListener directSelectColorListener;
+    private final OnSelectColorListener selectColorListener;
+    private final float tickSizeDimen;                 // when equals 0 (default used would be 24dp)
+    private final float cardViewDimen;                 // when equals 0 (default used would be 45dp)
+
+    public ColorPickerBottomSheetDialog(Context context,
+                                        int columns,
+                                        int defaultColor,
+                                        int itemDrawableRes,
+                                        int tickColor,
+                                        int dividerViewColor,
+                                        ColorItemShape colorShape,
+                                        ArrayList<ColorPaletteItemModel> colorsList,
+                                        HashMap<Integer, Integer> colorItems,
+                                        boolean titleTextChanged,
+                                        String dialogTitle,
+                                        boolean positiveButtonTextChanged,
+                                        String dialogPositiveButtonText,
+                                        boolean negativeButtonTextChanged,
+                                        String dialogNegativeButtonText,
+                                        OnDirectSelectColorListener directSelectColorListener,
+                                        OnSelectColorListener selectColorListener,
+                                        boolean cardSizeChanged,
+                                        boolean tickSizeChanged,
+                                        float tickSizeDimen,
+                                        float cardViewDimen) {
         this.context = context;
-        colorsList = new ArrayList<>();
+        this.columns = columns;
+        this.defaultColor = defaultColor;
+        this.itemDrawableRes = itemDrawableRes;
+        this.tickColor = tickColor;
+        this.dividerViewColor = dividerViewColor;
+        this.colorShape = colorShape;
+        this.colorsList = colorsList;
+        this.colorItems = colorItems;
+        this.titleTextChanged = titleTextChanged;
+        this.positiveButtonTextChanged = positiveButtonTextChanged;
+        this.negativeButtonTextChanged = negativeButtonTextChanged;
+        this.dialogTitle = dialogTitle != null ? dialogTitle : context.getString(R.string.dialog_title);
+        this.dialogPositiveButtonText = dialogPositiveButtonText != null ? dialogPositiveButtonText : context.getString(R.string.dialog_positive_button_text);
+        this.dialogNegativeButtonText = dialogNegativeButtonText != null ? dialogNegativeButtonText : context.getString(R.string.dialog_negative_button_text);
+        this.directSelectColorListener = directSelectColorListener;
+        this.selectColorListener = selectColorListener;
+        this.cardSizeChanged = cardSizeChanged;
+        this.tickSizeChanged = tickSizeChanged;
+        this.tickSizeDimen = tickSizeDimen;
+        this.cardViewDimen = cardViewDimen;
+
         bottomSheetDialogView = LayoutInflater.from(context).inflate(R.layout.layout_color_palette_bottomsheet, null, false);
         colorPaletteRelLayout = bottomSheetDialogView.findViewById(R.id.colorPaletteRelLayout);
         recyclerViewColors = bottomSheetDialogView.findViewById(R.id.recyclerViewColors);
@@ -85,9 +130,6 @@ public class ColorPickerBottomSheetDialog implements OnColorItemClickListener {
         negativeButton = bottomSheetDialogView.findViewById(R.id.negativeButton);
         dialogTitleText = bottomSheetDialogView.findViewById(R.id.dialogTitleText);
         dividerView = bottomSheetDialogView.findViewById(R.id.dividerView);
-        dialogTitle = context.getString(R.string.dialog_title);
-        dialogPositiveButtonText = context.getString(R.string.dialog_positive_button_text);
-        dialogNegativeButtonText = context.getString(R.string.dialog_negative_button_text);
     }
 
     @Override
@@ -106,9 +148,6 @@ public class ColorPickerBottomSheetDialog implements OnColorItemClickListener {
     public void show() {
         if (context == null) {
             return;
-        }
-        if (colorsList == null || colorsList.isEmpty()) {
-            setColors();
         }
         if (itemDrawableRes != 0) {
             colorAdapter = new ColorAdapter(colorsList, context, itemDrawableRes, this);
@@ -184,224 +223,6 @@ public class ColorPickerBottomSheetDialog implements OnColorItemClickListener {
     }
 
     /**
-     * On using OnSelectColorListener the bottom sheet dialog box would have the positive and negative buttons.
-     * It would be fired on pressing either of the buttons.
-     */
-    public ColorPickerBottomSheetDialog setOnSelectColorListener(OnSelectColorListener selectColorListener) {
-        this.selectColorListener = selectColorListener;
-        return this;
-    }
-
-    /**
-     * On using onDirectSelectColorListener the bottom sheet dialog box would not have the positive and negative buttons.
-     * It would be fired as soon as a color is pressed.
-     */
-    public ColorPickerBottomSheetDialog setOnDirectSelectColorListener(OnDirectSelectColorListener directSelectColorListener) {
-        this.directSelectColorListener = directSelectColorListener;
-        return this;
-    }
-
-    /**
-     * Sets the colors from array defined in this library (arrays.xml).
-     * In total 15 default colors would be added.
-     *
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setColors() {
-        if (context == null) {
-            return this;
-        }
-        TypedArray typedArray = context.getResources().obtainTypedArray(R.array.default_colors);
-        for (int i = 0; i < typedArray.length(); i++) {
-            colorsList.add(new ColorPaletteItemModel(typedArray.getColor(i, 0), false));
-        }
-        typedArray.recycle();
-        return this;
-    }
-
-    /**
-     * Sets the colors from array defined in the arrays.xml of app.
-     * For example, see arrays.xml of this library.
-     *
-     * @param resId (Array resource)
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setColors(int resId) {
-        if (context == null) {
-            return this;
-        }
-        TypedArray typedArray = context.getResources().obtainTypedArray(resId);
-        for (int i = 0; i < typedArray.length(); i++) {
-            colorsList.add(new ColorPaletteItemModel(typedArray.getColor(i, 0), false));
-        }
-        typedArray.recycle();
-        return this;
-    }
-
-    /**
-     * Sets the colors from arrayList of hex values (strings).
-     *
-     * @param colorsHexList (ArrayList of Strings)
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setColors(ArrayList<String> colorsHexList) {
-        for (String colors : colorsHexList) {
-            int color = Color.parseColor(colors);
-            colorsList.add(new ColorPaletteItemModel(color, false));
-        }
-        return this;
-    }
-
-    /**
-     * Sets the colors from int values of colors.
-     * For example, Color.BLUE or Color.parseColor("#000000").
-     *
-     * @param colors (list of colors int value)
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setColors(int... colors) {
-        for (int color : colors) {
-            colorsList.add(new ColorPaletteItemModel(color, false));
-        }
-        return this;
-    }
-
-    /**
-     * Set the shape of color item. SQUARE and CIRCLE are the two shapes available.
-     * By default, SQUARE would be selected.
-     *
-     * @param colorShape (shape of color item)
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setColorItemShape(ColorItemShape colorShape) {
-        this.colorShape = colorShape;
-        return this;
-    }
-
-    /**
-     * Sets the drawable of item in color palette (for any shape other than square and circle are
-     * also allowed).
-     *
-     * @param itemDrawable (Drawable resource)
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setColorItemDrawable(int itemDrawable) {
-        this.itemDrawableRes = itemDrawable;
-        return this;
-    }
-
-    /**
-     * Set the value of columns. This value would be used in spanCount of GridLayoutManager.
-     *
-     * @param columns (column count)
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setColumns(int columns) {
-        this.columns = columns;
-        return this;
-    }
-
-    /**
-     * Sets the default color when bottom sheet dialog pops up.
-     * Default color would have a tick mark on the color.
-     *
-     * @param defaultColor (default color int value)
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setDefaultSelectedColor(int defaultColor) {
-        this.defaultColor = defaultColor;
-        return this;
-    }
-
-    /**
-     * Sets the default color when bottom sheet dialog pops up.
-     * Default color would have a tick mark on the color.
-     *
-     * @param defaultColor (default color int value)
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setDefaultSelectedColor(String defaultColor) {
-        this.defaultColor = Color.parseColor(defaultColor);
-        return this;
-    }
-
-    /**
-     * Sets the color of tick mark on item in color palette on every color item selected.
-     * Default color is white.
-     *
-     * @param tickColor (tick color on item in palette)
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setTickColor(int tickColor) {
-        this.tickColor = tickColor;
-        return this;
-    }
-
-    /**
-     * Sets the color of tick mark on particular items in color palette. These items would have
-     * the color passed in this method.
-     * Default color is white.
-     *
-     * @param tickColor (tick color on item in palette)
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setTickColor(int tickColor, int... colorItems) {
-        this.tickColor = tickColor;
-        this.colorItems = new HashMap<>();
-        for (int item : colorItems) {
-            this.colorItems.put(item,item);
-        }
-        return this;
-    }
-
-    /**
-     * Sets the color of divider (below the title of dialog).
-     *
-     * @param backgroundColor (color)
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setDividerViewColor(int backgroundColor) {
-        this.dividerViewColor = backgroundColor;
-        return this;
-    }
-
-    /**
-     * Sets the title of dialog box. Default title is "Choose Color".
-     *
-     * @param dialogTitle (Title of bottom sheet dialog box)
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setDialogTitle(String dialogTitle) {
-        this.dialogTitle = dialogTitle;
-        titleTextChanged = true;
-        return this;
-    }
-
-    /**
-     * Sets the Positive button text of dialog box. Default text is "Ok".
-     *
-     * @param dialogPositiveButtonText (Positive button text)
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setPositiveButtonText(String dialogPositiveButtonText) {
-        this.dialogPositiveButtonText = dialogPositiveButtonText;
-        positiveButtonTextChanged = true;
-        return this;
-    }
-
-    /**
-     * Sets the Negative button text of dialog box. Default text is "Cancel".
-     *
-     * @param dialogNegativeButtonText (Negative button text)
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setNegativeButtonText(String dialogNegativeButtonText) {
-        this.dialogNegativeButtonText = dialogNegativeButtonText;
-        negativeButtonTextChanged = true;
-        return this;
-    }
-
-    /**
      * Get the positive button from bottom sheet dialog box.
      *
      * @return positiveButton
@@ -467,28 +288,75 @@ public class ColorPickerBottomSheetDialog implements OnColorItemClickListener {
         }
     }
 
-    /**
-     * Sets the height and width of color items in palette in dp.
-     * Default Value is 45dp
-     *
-     * @param dimen (in dp)
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setColorItemDimenInDp(int dimen) {
-        cardSizeChanged = true;
-        this.cardViewDimen = dimen;
-        return this;
-    }
+    public static class Builder extends ColorPickerBuilder<ColorPickerBottomSheetDialog.Builder> {
 
-    /**
-     * Sets the tick mark size in dp. Default value is 24dp.
-     *
-     * @param dimen (in dp)
-     * @return this
-     */
-    public ColorPickerBottomSheetDialog setTickDimenInDp(int dimen) {
-        tickSizeChanged = true;
-        this.tickSizeDimen = dimen;
-        return this;
+        protected int dividerViewColor = 0;
+        protected boolean positiveButtonTextChanged;
+        protected boolean negativeButtonTextChanged;
+        protected boolean titleTextChanged;
+
+        public Builder(Context context) {
+            super(context);
+        }
+
+        public ColorPickerBottomSheetDialog build() {
+            return new ColorPickerBottomSheetDialog(
+                    context,
+                    columns,
+                    defaultColor,
+                    itemDrawableRes,
+                    tickColor,
+                    dividerViewColor,
+                    colorShape,
+                    colorsList,
+                    colorItems,
+                    titleTextChanged,
+                    dialogTitle,
+                    positiveButtonTextChanged,
+                    dialogPositiveButtonText,
+                    negativeButtonTextChanged,
+                    dialogNegativeButtonText,
+                    directSelectColorListener,
+                    selectColorListener,
+                    cardSizeChanged,
+                    tickSizeChanged,
+                    tickSizeDimen,
+                    cardViewDimen
+            );
+        }
+
+        public void show() {
+            build().show();
+        }
+
+
+        /**
+         * Sets the color of divider (below the title of dialog).
+         *
+         * @param backgroundColor (color)
+         * @return this
+         */
+        public Builder setDividerViewColor(int backgroundColor) {
+            this.dividerViewColor = backgroundColor;
+            return this;
+        }
+
+        @Override
+        public Builder setDialogTitle(String dialogTitle) {
+            titleTextChanged = true;
+            return super.setDialogTitle(dialogTitle);
+        }
+
+        @Override
+        public Builder setPositiveButtonText(String dialogPositiveButtonText) {
+            positiveButtonTextChanged = true;
+            return super.setPositiveButtonText(dialogPositiveButtonText);
+        }
+
+        @Override
+        public Builder setNegativeButtonText(String dialogNegativeButtonText) {
+            negativeButtonTextChanged = true;
+            return super.setNegativeButtonText(dialogNegativeButtonText);
+        }
     }
 }
